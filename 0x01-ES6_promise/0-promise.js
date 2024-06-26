039f0143aa98673a6b7f88d18500442e1ee9a223
“Dawitt-dev” <davoza27@yahoo.com>
2024-06-26 22:45:58 +0000

    task_0. Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // This is where you would typically make an API call.
+    // For now, let's just resolve the promise immediately.
+    resolve('API response');
+  });
+}
+
+export default getResponseFromAPI;
