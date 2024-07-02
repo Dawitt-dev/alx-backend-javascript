a23f38caef5e310180ca7ee65556beea1a5a76c8
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-02 22:51:08 +0000

    6. Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+// Define the function setFromArray
+function setFromArray(array) {
+  // Create and return a new Set from the array
+  return new Set(array);
+}
+
+// Export the function (if using ES6 modules)
+export default setFromArray;
