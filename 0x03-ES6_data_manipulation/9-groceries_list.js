33b9d9686248b2cdb68e0c068d01d6ad0cb19854
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-02 23:01:52 +0000

    9. Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+// Define the function groceriesList
+function groceriesList() {
+  // Create and return a new Map with the specified items
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
+
+// Export the function (if using ES6 modules)
+export default groceriesList;
