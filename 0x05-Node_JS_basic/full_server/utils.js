02275fdb82f00c024d8f82e7b662a92e70985e52
“Dawitt-dev” <davoza27@yahoo.com>
2024-08-20 22:31:43 +0000

    task_8

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const students = {};
+    const lines = data.split('\n').filter((line) => line.trim());
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (field) {
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+    });
+
+    resolve(students);
+  });
+});
+
+export default readDatabase;
