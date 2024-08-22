dfb7d2f95525f906305b362da25bb271e5624b34
“Dawitt-dev” <davoza27@yahoo.com>
2024-08-22 14:16:22 +0000

    task_3. Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,18 +1,17 @@
 // 3-payment.test.js
 const sinon = require('sinon');
-const expect = require('chai').expect;
-const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
+const { expect } = require('chai');
+const { sendPaymentRequestToApi } = require('./3-payment.js');
+const { Utils } = require('./utils.js');
 
-describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with SUM, 100, 20', () => {
-    const spy = sinon.spy(Utils);
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with SUM, 100, 20', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);	 
-    spy.calculateNumber.restore(); // Always restore the spy after the test
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore(); // Always restore the spy after the test
   });
 });
 
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,24 +1,19 @@
 // utils.js
 const Utils = {
-  calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-
-  if (type === 'SUM') {
-    return roundedA + roundedB;
-  }
-
-  if (type === 'SUBTRACT') {
-    return roundedA - roundedB;
-  }
-
-  if (type === 'DIVIDE') {
-    if (roundedB === 0) {
-      return 'Error';
+  calculateNumber: function (type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
     }
-    return roundedA / roundedB;
+    throw new Error('Invalid operation type');
   }
+};
 
-  throw new Error('Invalid operation type');
-}
-module.exports = Utils;
+module.exports = { Utils };
+
