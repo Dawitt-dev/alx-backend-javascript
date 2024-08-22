68e3bfa43efd6d5044179f867aaaa2e3bd9ce4da
“Dawitt-dev” <davoza27@yahoo.com>
2024-08-22 14:54:45 +0000

    task_8. Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,24 @@
+// api.test.js
+
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', () => {
+  const url = 'http://localhost:7865/';
+
+  it('Correct status code?', (done) => {
+    request(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request(url, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
